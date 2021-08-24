1862a462b38175c47874b12a6a120d01a73f0cd8
IannDang <iann1978@163.com>
2021-08-24 15:54:32 +0800

    + (airport_service) Add parameter username&passord in login.

--- airport_service/src/main.go
+++ airport_service/src/main.go
@@ -18,9 +18,43 @@ func headers(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func login(w http.ResponseWriter, req *http.Request) {
+
+	keys, ok := req.URL.Query()["key"]
+
+	if !ok || len(keys[0]) < 1 {
+		fmt.Fprintf(w,"Url Param 'key' is missing!!!")
+		return
+	}
+
+	key := keys[0]
+	fmt.Fprintf(w, "key: %v\n", key)
+
+	usernames, ok := req.URL.Query()["username"]
+	if !ok || len(usernames[0]) < 1 {
+		fmt.Fprintf(w, "Url Param 'username' is missing!!!")
+		return
+	}
+	username := usernames[0]
+	fmt.Fprintf(w, "username: %v\n", username)
+
+
+	passwords, ok := req.URL.Query()["password"]
+	if !ok || len(passwords[0]) < 1 {
+		fmt.Fprintf(w, "Url Param 'password' is missing!!!")
+		return
+	}
+	password := passwords[0]
+	fmt.Fprintf(w, "password: %v\n", password)
+
+	fmt.Fprintf(w, "Succeed!")
+}
+
+
 func main() {
 	http.HandleFunc("/hello", hello)
 	http.HandleFunc("/headers", headers)
+	http.HandleFunc("/login", login)
 
 	http.ListenAndServe(":8099", nil)
 }
