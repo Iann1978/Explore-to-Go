64d217691826be647373dc27358bcf005913db55
IannDang <iann1978@163.com>
2021-09-06 16:44:11 +0800

    - (airport_service) Clean.

--- airport_service/src/main2.go
+++ airport_service/src/main2.go
@@ -3,13 +3,10 @@ package main
 import (
 	"airport_service/myfmt1"
 	"airport_service/user"
-	"database/sql"
 	"errors"
 	"fmt"
 	"net"
 	"net/http"
-
-	_ "github.com/mattn/go-sqlite3"
 )
 
 var users user.UserDatabase
@@ -60,27 +57,6 @@ func main() {
 	fmt.Println("haha")
 	myfmt1.Println()
 	myprintf()
-
-	db, err := sql.Open("sqlite3", "./foo.db")
-	checkErr(err)
-
-	// stmt, err := db.Prepare("create table userinfo(username text, password text)")
-	// checkErr(err)
-
-	// _, err = stmt.Exec()
-	// checkErr(err)
-
-	// Insert the data
-	stmt, err := db.Prepare("insert into userinfo(username, password) values(?,?)")
-	checkErr(err)
-
-	res, err := stmt.Exec("astaxie", "Developer Department")
-	checkErr(err)
-
-	id, err := res.LastInsertId()
-	checkErr(err)
-
-	fmt.Println(id)
 
 	http.HandleFunc("/hello", hello)
 	//http.HandleFunc("/headers", headers)
