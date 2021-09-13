6f5adb5b5e9b630414d32bf8dbd8b1afc111081e
IannDang <iann1978@163.com>
2021-09-13 15:06:39 +0800

    + (server) Add TestConnection into testing.

--- airport_service/src/test/main_test.go
+++ airport_service/src/test/main_test.go
@@ -1,13 +1,42 @@
 package test
 
 import (
-	"testing"
+	"errors"
+	"fmt"
+	"net"
 	"net/http"
+	"testing"
 )
 
+func getClientIp() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, address := range addrs {
+
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String(), err
+			}
+		}
+	}
+
+	return "", errors.New("Can not find the client ip address!")
+}
 
 func TestConnection(t *testing.T) {
-	resp, err := http.Get("http://gobyexample.com")
+	ipstr, err := getClientIp()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	req := fmt.Sprintf("http://%s:8099/hello", ipstr)
+	fmt.Println(req)
+
+	resp, err := http.Get(req)
 	if err != nil {
 		t.Errorf(err.Error())
 	}
