e932a269f1c4bed01cf93e5408c9c07aff469e3d
IannDang <iann1978@163.com>
2021-09-14 14:38:17 +0800

    + (service) Add TestRegist into test.

--- airport_service/src/test/main_test.go
+++ airport_service/src/test/main_test.go
@@ -1,8 +1,10 @@
 package test
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"testing"
@@ -58,4 +60,72 @@ func TestRebuildDb(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 	defer resp.Body.Close()
+}
+
+type ErrorCode int32
+
+const (
+	NoError        ErrorCode = 0
+	UnknownError   ErrorCode = 1
+	ParameterError ErrorCode = 2
+	UserExist                = 3
+	UserNotExist             = 4
+)
+
+func (e ErrorCode) String() string {
+	switch e {
+	case NoError:
+		return "No Error"
+	case UnknownError:
+		return "Unknown Error"
+	case ParameterError:
+		return "Parameter Error"
+	case UserExist:
+		return "User Already Exist"
+	case UserNotExist:
+		return "User Not Exist"
+	}
+	return "unknown"
+}
+
+type RegistResp struct {
+	ErrorCode   ErrorCode
+	ErrorString string
+}
+
+func TestRegist(t *testing.T) {
+	ipstr, err := getClientIp()
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	req := fmt.Sprintf("http://%s:8099/regist?username=test0&&password=test0", ipstr)
+	fmt.Println(req)
+
+	resp, err := http.Get(req)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	fmt.Println("body:", string(body))
+
+	var registResp RegistResp
+
+	if err := json.Unmarshal(body, &registResp); err != nil {
+		t.Errorf(err.Error())
+		return
+	}
+
+	fmt.Println(registResp)
+
+	if NoError != registResp.ErrorCode {
+		t.Errorf(registResp.ErrorCode.String())
+	}
+
 }
