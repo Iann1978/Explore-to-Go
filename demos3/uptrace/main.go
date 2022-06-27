6ff6b56c2b986b1cd6bbc0d0aea90eff91b4fa12
iann <iann1978@163.com>
2022-06-27 09:33:19 +0800

    + (demos/uptrace) Add a demo uptrace.

---
+++ demos3/uptrace/main.go
@@ -0,0 +1,52 @@
+//https://get.uptrace.dev/guide/opentelemetry-tracing-tool.html#binaries
+//https://docs.uptrace.dev/guide/go.html#getting-started
+//https://get.uptrace.dev/guide/opentelemetry-tracing-tool.html#clickhouse
+
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+
+	"github.com/uptrace/uptrace-go/uptrace"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Configure OpenTelemetry with sensible defaults.
+	uptrace.ConfigureOpentelemetry(
+		// copy your project DSN here or use UPTRACE_DSN env var
+		// uptrace.WithDSN("https://<key>@uptrace.dev/<project_id>"),
+		uptrace.WithDSN("http://project2_secret_token@192.168.100.78:14317/2"),
+
+		uptrace.WithServiceName("myservice"),
+		uptrace.WithServiceVersion("1.0.0"),
+	)
+	// Send buffered spans and free resources.
+	defer uptrace.Shutdown(ctx)
+
+	// Create a tracer. Usually, tracer is a global variable.
+	tracer := otel.Tracer("app_or_package_name")
+
+	// Create a root span (a trace) to measure some operation.
+	ctx, main := tracer.Start(ctx, "main-operation")
+	// End the span when the operation we are measuring is done.
+	defer main.End()
+
+	// We pass the main (parent) span in the ctx so we can reconstruct a trace later.
+	_, child1 := tracer.Start(ctx, "child1-of-main")
+	child1.SetAttributes(attribute.String("key1", "value1"))
+	child1.RecordError(errors.New("error1"))
+	child1.End()
+
+	_, child2 := tracer.Start(ctx, "child2-of-main")
+	child2.SetAttributes(attribute.Int("key2", 42), attribute.Float64("key3", 123.456))
+	child2.End()
+
+	fmt.Printf("trace: %s\n", uptrace.TraceURL(main))
+}
